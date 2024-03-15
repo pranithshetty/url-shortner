c62d9f8168adcf2ab737cb89b2b25be8b6cd9730
pranithshetty <shetty.praneeth4@gmail.com>
2024-03-15 22:11:45 +0530

    Api addition for redirect using shortId

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 //URL Shortner entry
+const express = require("express");
+const urlRouter = require("./routes/url.routes");
+const connectMongoDb = require("./connection");
+const { URL } = require("./model/url.model");
+
+const app = express();
+const PORT = 3000;
+connectMongoDb("mongodb://127.0.0.1:27017/short-url");
+
+app.use(express.json());
+
+app.use("/url", urlRouter);
+
+app.get("/:shortId", async (req, res) => {
+	const shortId = req.params.shortId;
+	const entry = await URL.findOneAndUpdate(
+		{ shortId },
+		{
+			$push: {
+				visitHistory: { timestamp: Date.now() },
+			},
+		}
+	);
+	res.redirect(entry.redirectUrl);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on PORT::${PORT}`);
+});
