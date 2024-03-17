4e86543b20795c84675489ef7a5d46eeecd860b4
pranithshetty <shetty.praneeth4@gmail.com>
2024-03-17 21:49:46 +0530

    Static route to render home page

---
+++ routes/staticPage.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { URL } = require("../model/url.model");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+	const allUrls = await URL.find({});
+	return res.render("home", { urls: allUrls });
+});
+
+module.exports = router;
