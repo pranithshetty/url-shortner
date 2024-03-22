b1644f48704189b1c7bd7177ef15d2df99431dce
pranithshetty <shetty.praneeth4@gmail.com>
2024-03-22 05:59:10 +0530

    middleware update for token based authentication/authorization

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,35 @@
 const { getUser } = require("../services/auth");
 
+//authentication
+function checkForAuthentication(req, res, next) {
+	const tokenCookie = req.cookies?.token;
+	req.user = null;
+	if (!tokenCookie) return next();
+
+	const token = tokenCookie;
+	const user = getUser(token);
+	req.user = user;
+	return next();
+}
+
+//authorization
+function restrictTo(roles = []) {
+	return function (req, res, next) {
+		if (!req.user) {
+			return res.redirect("/login");
+		}
+		if (!roles.includes(req.user.role)) {
+			return res.end("Un-Authorized");
+		}
+		return next();
+	};
+}
+
+/* 
+* For authentication via cookies
+! cookies can cause issue when deploying on multiple devices 
+*/
+/*
 async function restrictToLoginUserOnly(req, res, next) {
 	const uuid = req.cookies?.uid;
 	if (!uuid) {
@@ -18,4 +48,6 @@ async function checkAuth(req, res, next) {
 	next();
 }
 
-module.exports = { restrictToLoginUserOnly, checkAuth };
+*/
+
+module.exports = { checkForAuthentication, restrictTo };
